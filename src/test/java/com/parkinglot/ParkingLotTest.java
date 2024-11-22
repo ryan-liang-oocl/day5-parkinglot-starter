c8070ddcf911da039fcaae90fb0dfbc730ed2875
Ryan Liang <ryan.liang@oocl.com>
2024-11-22 13:47:17 +0800

    [Story1]feat, Case3, return the right car when fetch the car by ticket

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotTest {
 
@@ -30,5 +29,34 @@ public class ParkingLotTest {
         Car myCar = parkingLot.fetch(ticket);
         //Then
         assertEquals(car, myCar);
+    }
+
+    @Test
+    public void should_return_null_when_fetch_given_a_wrong_ticket() {
+        //Given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        parkingLot.park(car);
+        Ticket ticket = new Ticket();
+        //When
+        Car otherCar = parkingLot.fetch(ticket);
+        //Then
+        assertNull(otherCar);
+    }
+    
+    @Test
+    public void should_return_right_car_when_fetch_given_tickets() {
+        //Given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Ticket ticket1 = parkingLot.park(car1);
+        Ticket ticket2 = parkingLot.park(car2);
+        //When
+        Car previousCar = parkingLot.fetch(ticket1);
+        Car laterCar = parkingLot.fetch(ticket2);
+        //Then
+        assertEquals(car1, previousCar);
+        assertEquals(car2, laterCar);
     }
 }
